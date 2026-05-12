2d4d633b83b913153c7c12c4d1adf7721c538ab5
tyson3636 <emmanueltyson36@gmail.com>
2026-05-12 14:39:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
-  },
   server: {
-    port: 3000,
-    host: true,
-  },
-});
+    allowedHosts: ['smilehospital.onrender.com']
+  }
+})
